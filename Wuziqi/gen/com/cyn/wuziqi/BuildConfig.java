830c0e11a2d1dae0b69792ae039faf47a6a3992c
ladycui <lady_cui@126.com>
2016-10-19 21:23:05 +0800

    1st commit Wuziqi

---
+++ Wuziqi/gen/com/cyn/wuziqi/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.cyn.wuziqi;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Wuziqi/src/com/cyn/wuziqi/MainActivity.java
@@ -0,0 +1,36 @@
+package com.cyn.wuziqi;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+	private WuziqiPanel wuziqiPanel;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wuziqiPanel=(WuziqiPanel)findViewById(R.id.id_wuziqi);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        
+        int id = item.getItemId();
+        if (id == R.id.action_again) {
+        	wuziqiPanel.start();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
